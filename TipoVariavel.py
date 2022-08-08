9d5f50ca93efd7195047cf5450d26323d7f29e60
Carol Alves <prof.carolalves90@gmail.com>
2022-08-08 13:54:17 -0300

    :sparkles: adicionando tipo de variavel

---
+++ TipoVariavel.py
@@ -0,0 +1,14 @@
+codigo = 10
+salario = 1500.00
+nome = 'Jose'
+situação = True
+
+tipo = type (salario)
+
+print(salario)
+print(tipo)
+
+
+print("Codigo:", codigo, "Nome:", nome, "O salario atual e de", salario)
+
+print("Codigo: "+ str (codigo)+ " Nome: "+ nome+ " O salario atual e de "+ str(salario))
