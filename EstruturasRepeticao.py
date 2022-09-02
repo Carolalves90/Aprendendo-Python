5c178cd268ac7501df92c0e374e536dbaad2df29
Carol Alves <prof.carolalves90@gmail.com>
2022-09-02 14:24:42 -0300

    adicionando estruturas de repetição

---
+++ EstruturasRepeticao.py
@@ -0,0 +1,26 @@
+# FOR
+for n in range(10):
+    print(n)
+
+print("Fim da contagem")
+print(" ")
+
+for n in range(5,16):
+    print(n)
+
+print("Fim da contagem, iniciando em 5")
+print(" ")
+
+for n in range(10, 0, -1):
+    print(n)
+
+print("Fim da contagem em decremento ")
+print(" ")
+
+#WHILE
+x = 1
+while x<=15:
+    print(x)
+    x=x+1
+
+print("Fim da contagem usando While")
